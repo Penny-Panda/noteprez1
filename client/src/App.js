87ff2f860dccba21e147896ba66a712f9b0cdfd0
Ishan Pandey <191.imp@gmail.com>
2022-01-18 19:33:06 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import { useDispatch } from 'react-redux'; 
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/posts';
 import Form from './components/Form/Form';
 
@@ -10,6 +12,12 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
         <Container maxWidth = "lg">
             <AppBar className={classes.appBar} position = "static" color = "inherit">
--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://ownos:admin123@noteprez.rzyp4.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
